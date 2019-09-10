5ed99dd7f846515a2cd85912d8c807cd886de760
Jorge Luis Malla <jmalla@ioet.com>
2019-09-10 11:22:09 -0500

    Init

---
+++ src/main/java/com/kerno/kernobase/KernoBaseApplication.java
@@ -0,0 +1,13 @@
+package com.kerno.kernobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KernoBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KernoBaseApplication.class, args);
+	}
+
+}
